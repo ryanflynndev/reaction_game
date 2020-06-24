29a99368c37e7a478428a2c00b3dfa32c9b810d4
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-24 11:01:43 -0400

    Added a background color, and a mouse event where a piece of text tells the user to click to begin.

--- reaction_game.rb
+++ reaction_game.rb
@@ -1,3 +1,13 @@
 require 'ruby2d'
 
+set background: 'green'
+
+begin_message = Text.new('Click to begin')
+
+on :mouse_down do |event|
+    # x and y coordinates of the mouse button event
+    puts event.x, event.y
+    begin_message.remove
+end
+
 show
