b109cb9337b51e7055ff2f9b35fe01d3be460e22
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-24 11:23:09 -0400

    Added game states and clicking functionality.

--- reaction_game.rb
+++ reaction_game.rb
@@ -1,13 +1,33 @@
 require 'ruby2d'
 
-set background: 'green'
+set background: 'red'
 
 begin_message = Text.new('Click to begin')
+game_started = false
+square = nil
 
 on :mouse_down do |event|
     # x and y coordinates of the mouse button event
     puts event.x, event.y
-    begin_message.remove
+
+    if game_started
+       if square.contains?(event.x, event.y)
+            puts "Clicked it!"
+       end
+    else
+        begin_message.remove
+    
+        square = Square.new(
+        x: 100, y: 200,
+        size: 125,
+        color: 'white',
+  
+        )
+
+        game_started = true
+    end
 end
+
+
 
 show
